8006f9d0597051b866d38fb6b3d5aaba6ec13208
Amit Kumar Bhuiyan <mr.amitkb@gmail.com>
2024-05-12 19:49:09 +0600

    Done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
     return (
         <header className='max-w-screen-xl mx-auto'>
             <div className='flex justify-between items-center border-b p-5'>
-                <h1 className="text-4xl font-bold">Knowledge Cafe</h1>
+                <h1 className="text-4xl font-bold"><span className='text-green-500'>Knowledge</span> Cafe</h1>
                 <img src={profile} alt="" />
             </div>
         </header>
